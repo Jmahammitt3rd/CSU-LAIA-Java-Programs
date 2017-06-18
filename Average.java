71812cdc937e2c1b03c5d677d82365ed2514ea1b
Jerry Mahammitt III <Jmahammittt3rd@gmail.com>
2017-06-17 20:33:49 -0400

    Add files via upload
    Java code for a program that calculates and displays the average of six numbers.

---
+++ Average.java
@@ -0,0 +1,28 @@
+// This program averages six numbers ranging between 1000
+
+public class Average
+{
+   public static void main(String[] args)
+   {
+   
+double num;
+double avg;
+double sum;
+    
+   int num1= 1;
+   int num2= 2;
+   int num3= 4;
+   int num4= 10;
+   int num5= 100;
+   int num6= 120;
+    
+    sum = (num1 + num2 + num3 + num4 + num5 + num6);
+    
+    avg = sum / 6;
+    
+     System.out.println(" Average= " + avg);
+ }
+}
+    
+    
+  
